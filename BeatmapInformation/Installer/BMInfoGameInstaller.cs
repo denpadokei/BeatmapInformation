bb50ff0c097fd9b46f25d48456f7e7591eaab891
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-02-25 01:17:41 +0900

    プロジェクト ファイルを追加します。

---
+++ BeatmapInformation/Installer/BMInfoGameInstaller.cs
@@ -0,0 +1,21 @@
+﻿using BeatmapInformation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+using SiraUtil;
+using BeatmapInformation.Views;
+
+namespace BeatmapInformation.Installer
+{
+    public class BMInfoGameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            this.Container.BindInterfacesAndSelfTo<BeatmapInformationViewController>().FromNewComponentAsViewController().AsCached();
+            this.Container.BindInterfacesAndSelfTo<BeatmapInformationController>().FromNewComponentOnNewGameObject(nameof(BeatmapInformationController)).AsCached().NonLazy();
+        }
+    }
+}
---
+++ BeatmapInformation/Views/BeatmapInformationViewController.cs
@@ -0,0 +1,133 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Zenject;
+
+namespace BeatmapInformation.Views
+{
+    [HotReload]
+    internal class BeatmapInformationViewController : BSMLAutomaticViewController, IInitializable
+    {
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プロパティ
+        // For this method of setting the ResourceName, this class must be the first class in the file.
+        public string ResourceName => string.Join(".", GetType().Namespace, GetType().Name);
+
+        /// <summary>曲のタイトル を取得、設定</summary>
+        private string songName_;
+        /// <summary>曲のタイトル を取得、設定</summary>
+        public string SongName
+        {
+            get => this.songName_;
+
+            set => this.SetProperty(ref this.songName_, value);
+        }
+
+        /// <summary>曲のサブタイトル を取得、設定</summary>
+        private string songSubName_;
+        /// <summary>曲のサブタイトル を取得、設定</summary>
+        public string SongSubName
+        {
+            get => this.songSubName_;
+
+            set => this.SetProperty(ref this.songSubName_, value);
+        }
+
+        /// <summary>スコア を取得、設定</summary>
+        private string score_;
+        /// <summary>スコア を取得、設定</summary>
+        public string Score
+        {
+            get => this.score_;
+
+            set => this.SetProperty(ref this.score_, value);
+        }
+
+        /// <summary>ランク を取得、設定</summary>
+        private string rank_;
+        /// <summary>ランク を取得、設定</summary>
+        public string Rank
+        {
+            get => this.rank_;
+
+            set => this.SetProperty(ref this.rank_, value);
+        }
+
+        /// <summary>曲のカバー画像 を取得、設定</summary>
+        private Texture2D cover_;
+        /// <summary>曲のカバー画像 を取得、設定</summary>
+        public Texture2D Cover
+        {
+            get => this.cover_;
+
+            set => this.SetProperty(ref this.cover_, value);
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // Unity Message
+        private void Awake()
+        {
+
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // オーバーライドメソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // パブリックメソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プライベートメソッド
+        /// <summary>
+        /// プロパティへ値をセットし、Viewへ通知します
+        /// </summary>
+        /// <typeparam name="T">プロパティの型</typeparam>
+        /// <param name="property">プロパティ用変数</param>
+        /// <param name="value">更新する値</param>
+        /// <param name="membername">このメソッドを呼んだメンバーの名前（自動挿入なので指定する必要なし）</param>
+        /// <returns>変更があったかどうか</returns>
+        private bool SetProperty<T>(ref T property, T value, [CallerMemberName]string membername = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(property, value)) {
+                return false;
+            }
+            property = value;
+            this.OnPropertyChanged(membername);
+            return true;
+        }
+
+        /// <summary>
+        /// プロパティの変更があったときに呼び出されます。Viewへの通知をここでキャンセルできます。
+        /// </summary>
+        /// <param name="propertyName">呼び出されたプロパティ名</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            HMMainThreadDispatcher.instance?.Enqueue(() =>
+            {
+                this.NotifyPropertyChanged(propertyName);
+            });
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // メンバ変数
+        
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // 構築・破棄
+        public void Initialize()
+        {
+            
+        }
+        #endregion
+    }
+}
