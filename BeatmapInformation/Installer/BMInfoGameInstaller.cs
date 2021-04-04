ffbda92ae45e2069b190b09250b48fca5a5fa803
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-04-04 17:16:07 +0900

    クリーン

--- BeatmapInformation/Installer/BMInfoGameInstaller.cs
+++ BeatmapInformation/Installer/BMInfoGameInstaller.cs
@@ -8,7 +8,8 @@ namespace BeatmapInformation.Installer
 {
     public class BMInfoGameInstaller : MonoInstaller
     {
-        public override void InstallBindings() {
+        public override void InstallBindings()
+        {
             this.Container.BindInterfacesAndSelfTo<BeatmapInformationViewController>().FromNewComponentAsViewController().AsSingle().NonLazy();
             this.Container.BindInterfacesAndSelfTo<AudioSpectrum>().FromNewComponentOnNewGameObject(nameof(AudioSpectrum)).AsCached();
             this.Container.BindInterfacesAndSelfTo<TextFormatter>().AsSingle();
--- BeatmapInformation/Models/TextFormatter.cs
+++ BeatmapInformation/Models/TextFormatter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VRUIControls;
-using Zenject;
+﻿using System.Text;
 
 namespace BeatmapInformation.Models
 {
