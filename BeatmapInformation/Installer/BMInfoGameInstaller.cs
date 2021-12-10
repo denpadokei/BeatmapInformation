0e7a1672d0ca6bc15c47b03be48913fa5ddb8fd5
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-12-11 01:48:56 +0900

    fix 1.19.0

--- BeatmapInformation/Installer/BMInfoGameInstaller.cs
+++ BeatmapInformation/Installer/BMInfoGameInstaller.cs
@@ -2,6 +2,7 @@
 using BeatmapInformation.Models;
 using BeatmapInformation.Views;
 using SiraUtil;
+using UnityEngine;
 using Zenject;
 
 namespace BeatmapInformation.Installer
@@ -11,7 +12,7 @@ namespace BeatmapInformation.Installer
         public override void InstallBindings()
         {
             this.Container.BindInterfacesAndSelfTo<BeatmapInformationViewController>().FromNewComponentAsViewController().AsSingle().NonLazy();
-            this.Container.BindInterfacesAndSelfTo<AudioSpectrum>().FromNewComponentOnNewGameObject(nameof(AudioSpectrum)).AsCached();
+            this.Container.BindInterfacesAndSelfTo<AudioSpectrum>().FromNewComponentOn(new GameObject(nameof(AudioSpectrum))).AsCached();
             this.Container.BindInterfacesAndSelfTo<TextFormatter>().AsSingle();
             this.Container.BindMemoryPool<ScoreEntity, ScoreEntity.Pool>().WithInitialSize(32).AsCached();
         }
